f1dea4c45b2560a6c0d277cebf26d1a2e3a63852
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-12-28 02:22:48 +0100

    20.3

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import getCountries from './actions/actions-countries';
+import DevTools from './DevTools';
+
+render(
+    <Provider store={store}>
+        
+       <DevTools />
+    </Provider>,
+    document.getElementById('root')
+);
